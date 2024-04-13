b829537d6b3e8001d282ef3ae246aef53c9729a9
kelvinpompey <kelvin.pompey@gmail.com>
2024-04-13 14:50:31 -0400

    Initial commit

---
+++ apps/next/app/user/[id]/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import { UserDetailScreen } from 'app/features/user/detail-screen'
+
+export const runtime = 'edge'
+
+export default UserDetailScreen
---
+++ packages/app/features/user/detail-screen.tsx
@@ -0,0 +1,21 @@
+import { Button, Paragraph, YStack } from '@my/ui'
+import { ChevronLeft } from '@tamagui/lucide-icons'
+import React from 'react'
+import { useParams } from 'solito/navigation'
+import { useLink } from 'solito/navigation'
+
+export function UserDetailScreen() {
+  const params = useParams<{ id: string }>()
+  const link = useLink({
+    href: '/',
+  })
+
+  return (
+    <YStack f={1} jc="center" ai="center" gap="$4">
+      <Paragraph ta="center" fow="700">{`User ID: ${params.id}`}</Paragraph>
+      <Button {...link} icon={ChevronLeft}>
+        Go Home
+      </Button>
+    </YStack>
+  )
+}
